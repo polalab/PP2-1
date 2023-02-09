4b2e9e403a279bf0c64839a3fccdd1fc7c298105
polalab <58389054+polalab@users.noreply.github.com>
2023-02-10 00:00:57 +0100

    initial commit - excercises

---
+++ main.py
@@ -0,0 +1,24 @@
+# exercise 1 - create two variables a, b of type int and add them together
+
+# exercise 2 - take an input "c" from the user and print multiplication of a times c
+
+# excersise 3 - types - print type of every variable (type checking)
+
+a_int = 5
+b_bool = True
+c_string = "Hello,Katrina"
+d_float = 0.001
+
+# excercise 4 - make an algorithm that will print c_string a_int times
+
+
+# excercise 5 - make a function that will take a parameter 'num' of type int and return a string consisting of num times
+# 'hello' e.g. for num = 3 it will return 'hello hello hello'
+
+# excercise 6 - amend commit so that the function in excercise 5 takes two parameters - num and name and returns a string
+# consisting of num times 'hello' followed by a ', name'
+
+
+
+
+# *** If times allows: random library
